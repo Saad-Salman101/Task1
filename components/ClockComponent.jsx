0ca0fd860d80c12a8d69bb629a357e16e9397ba2
Saad Salman <saad.salman98@gmail.com>
2023-06-12 05:44:31 -0700

    2nd commit

--- components/ClockComponent.jsx
+++ components/ClockComponent.jsx
@@ -28,10 +28,10 @@ const ClockComponent = () => {
     <div>
 
         {locationData && (
-                    <div className='flex flex-row items-center text-[10px]  w-[200px] md:w-[18vw] md:mb-0 mb-7 '>
+                    <div className='flex flex-row items-center justify-end text-[10px]  w-[220px] md:w-[18vw] md:mb-0 mb-7 '>
                     <div className='flex justify-end flex-col w-full'>
-                  <div className='flex flex-row uppercase justify-end tracking-[2px] text-right'>{getCurrentTime()} {locationData.city} {locationData.country_name}</div>
-                    <div className='flex flex-row text-golden uppercase w-full justify-end tracking-[1px] text-right  '>{getCurrentDate()}</div>
+                  <div className='flex flex-row uppercase justify-end tracking-[2px] text-right text-[8px] md:text-[10px]'>{getCurrentTime()} {locationData.city} {locationData.country_name}</div>
+                    <div className='flex flex-row text-golden uppercase w-full justify-end tracking-[1px] text-right  text-[8px] md:text-[10px] '>{getCurrentDate()}</div>
                     </div>
                         <p className='mx-5'>
                           <CountryFlag countryCode={locationData.country_code} svg           style={{
