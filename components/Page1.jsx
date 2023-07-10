bfa1b03d2981d80875b379c6580f0c2563ae417e
Saad Salman <saad.salman98@gmail.com>
2023-07-10 09:33:04 -0700

    responsiveness changes

--- components/Page1.jsx
+++ components/Page1.jsx
@@ -28,7 +28,7 @@ const Page1 = () => {
 
     <div className='flex flex-col w-full justify-start items-end mt-2 '>
       <div className='flex flex-row w-full  justify-between items-center mt- 2xl:mt-14 '>
-        <div className='flex flex-col justify-start items-start ml-0 md:ml-5 flex-1  '><div> <img src='/assets/ZIDOC_Logo_Black_CMYK.svg' alt='' className='2xl:w-[129px] w-[100px] ml-4'/> </div>  </div>
+        <div className='flex flex-col justify-start items-start ml-0 md:ml-5 flex-1  '><div className='relative'> <img src='/assets/ZIDOC_Logo_Black_CMYK.svg' alt='' className='2xl:w-[129px] w-[100px] ml-4'/><img src='/assets/SecurityLockB.svg' alt='' className='w-[5px] absolute right-0 top-[-10px]'/> </div>  </div>
         <div className='flex flex-col justify-center items-center flex-1 '> <img src='/assets/ZigBlack.svg' alt='' className=' 2xl:w-[240px] w-[200px] mr-5 ' /></div>
         <div className='flex flex-col justify-between items-end mt-3 mr-10 flex-1'>
           <div className='flex flex-row justify-center items-center'><div className='mr-2 2xl:mr-8'><ClockComponent /> </div> 
@@ -37,14 +37,14 @@ const Page1 = () => {
       </div>
       <div className='flex justify-end items-center mt-5 '>
         <div className=' flex-col flex items-end mr-10 '><img src='/assets/PoweredbyZIMOAIB.png' alt='' className='w-[183px] ' />
-        <div>{sidebarstate === 'off' && <img src='/assets/TryForFree.png' alt='' className='mt-5 w-[123px]' />} </div> </div>
+        <div className='h-[20px]'>{sidebarstate === 'off' && <img src='/assets/TryForFree.png' alt='' className='mt-5 w-[123px]' />} </div> </div>
       </div>
     </div>
         
         <div className=' w-full flex flex-row justify-end items-center '>
             
-            <div className='flex justify-end relative 2xl:h-[846px]  2xl: w-[441px] h-[700px] mt-5'><img src='/assets/MAli-EXAMPLE.png' alt='' className=' w-full h-full rounded-2xl' />
-            <img src='/assets/SidePicSign.png' alt='' className='absolute right-7 top-2 md:w-[90%] w-[80%]'/>
+            <div className='flex justify-end relative w-[600px] 3xl:w-[700px] mt-5 ml-20 sm:ml-0'><img src='/assets/MAli-EXAMPLE.png' alt='' className=' w-full h-full rounded-2xl mt-5' />
+            <img src='/assets/SidePicSign.png' alt='' className='absolute right-7 top-7 md:w-[90%] w-[80%]'/>
             </div>
             
            
--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -116,7 +116,7 @@ const sidebaroff = () => {
 
             <div className='w-full uppercase text-[10px] text-white font-normal text-right tracking-[2px] cursor-pointer my-2 2xl:mt-3' onClick={() => handleSidebarItemClick('/contracts')}> contracts</div>
             <div>
-            <div className='w-full uppercase text-[10px] text-white font-light text-left tracking-[1px] cursor-pointer my-2 2xl:mt-3 2xl:mb-2' onClick={() => handleSidebarItemClick('/contractagreement')}> contract/agreement management</div>
+            <div className='w-full uppercase text-[10px] text-white font-light text-right tracking-[1px] cursor-pointer my-2 2xl:mt-3 2xl:mb-2' onClick={() => handleSidebarItemClick('/contractagreement')}> contract/agreement management</div>
               <div className='w-full uppercase text-[9px] text-gray-600 font-semibold text-right tracking-[2px] my-1'>organised, automated document work flows</div>
             </div>
             <div>
@@ -136,7 +136,7 @@ const sidebaroff = () => {
 
             <div className='w-full uppercase text-[10px] text-white font-light text-right tracking-[2px] cursor-pointer my-1' onClick={() => handleSidebarItemClick('/more')}> more</div>
             <div>
-            <div className='w-full uppercase text-[10px] text-white font-light text-left tracking-[2px] cursor-pointer' onClick={() => handleSidebarItemClick('/payment')}> payemnts </div>
+            <div className='w-full uppercase text-[10px] text-white font-light text-left tracking-[2px] cursor-pointer' onClick={() => handleSidebarItemClick('/payment')}> payements </div>
               <div className='w-full uppercase text-[9px] text-gray-600 font-semibold text-left tracking-[2px] my-1'> agree, sign and make payment(s) in a single click</div>
             </div>
             <div>
@@ -148,7 +148,7 @@ const sidebaroff = () => {
               <div className='w-full uppercase text-[9px] text-gray-600 font-semibold text-left tracking-[2px] my-1'> incorporate data collection and speed  up signing</div>
             </div>
 
-            <div className='w-full uppercase text-[10px] text-white font-light text-right tracking-[2px] flex flex-row justify-end  cursor-pointer my-2 2xl:mt-2 2xl:my-0' onClick={() => handleSidebarItemClick('/allproducts')}> all products <img src='/assets/SideBarRightArrow.png' alt='' className='ml-5' /> </div>
+            <div className='w-full uppercase text-[10px] text-white font-light text-right tracking-[2px] flex flex-row justify-end  cursor-pointer mt-6' onClick={() => handleSidebarItemClick('/allproducts')}> all products <img src='/assets/SideBarRightArrow.png' alt='' className='ml-5' /> </div>
 
 
             </div>
