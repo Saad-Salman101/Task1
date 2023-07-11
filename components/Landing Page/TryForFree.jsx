f2be18434c2d84bd6341f463da583684aa8332a9
Saad Salman <saad.salman98@gmail.com>
2023-07-11 09:00:04 -0700

    responsiveness changes

--- components/Landing Page/TryForFree.jsx
+++ components/Landing Page/TryForFree.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TryForFree = () => {
   return (
-    <div className='text-[20px] tracking-[2px] font-bold mt-4 ml-1 uppercase '>Try For Free</div>
+    <div className='text-[20px] tracking-[2px] font-bold mt-7 ml-1 uppercase '>Try For Free</div>
   )
 }
 
