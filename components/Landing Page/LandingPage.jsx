09ee232afa9d5777ce692cce72a8414f3ef915f8
Saad Salman <saad.salman98@gmail.com>
2023-07-11 22:33:32 -0700

    add different sidebar tabs

--- components/Landing Page/LandingPage.jsx
+++ components/Landing Page/LandingPage.jsx
@@ -2,11 +2,18 @@
 import React,{useState} from 'react'
 import ClockComponent from '../LocAndClock/ClockComponent'
 import SideBar from '../SideBar/SideBar'
+import SideBar1 from '../SideBar/SideBarProduct'
 
 // import  FaTachometerAlt from 'react-icons/fa';
 import { FaBars } from 'react-icons/fa';
 import { useSelector } from "react-redux";
 import TryForFree from './TryForFree';
+import SideBarSol from '../SideBar/SideBarSol';
+import SideBarDoc from '../SideBar/SideBarDoc';
+import SideBarES from '../SideBar/SideBarES';
+import SideBarIndustry from '../SideBar/SideBarIndustry';
+import SideBarEncrypt from '../SideBar/SideBarEncrypt';
+import SideBarPricing from '../SideBar/SideBarPricing';
 
 const Page1 = () => {
     const [collapsed, setCollapsed] = useState(false);
@@ -21,14 +28,23 @@ const Page1 = () => {
     
   return (
     <div className='flex flex-row  min-h-screen overflow-y-hidden'>
-        <SideBar onClick={() => console.log("hello")} />
 
+      <div className='relative h-full'>
+
+      <div className=' absolute top-0 left-0'><SideBar1/> </div>
+       <div className='absolute top-[20px] 2xl:top-[13px] left-0'> <SideBarSol/> </div>
+       <div className=' absolute top-[185px] 2xl:top-[232px] left-0'><SideBarDoc/> </div>
+       <div className=' absolute top-[325px] 2xl:top-[410px] left-0'><SideBarES/> </div>
+       <div className=' absolute top-[512px] 2xl:top-[633px] left-0'><SideBarIndustry/> </div>
+       <div className=' absolute top-[609px] 2xl:top-[760px] left-0'><SideBarEncrypt/> </div> 
+       <div className=' absolute top-[814px] 2xl:top-[980px] left-0'><SideBarPricing/> </div> 
+        </div>
     <div className=' w-full flex flex-col justify-start '>
         
 
     <div className='flex flex-col w-full justify-start items-end mt-2 '>
       <div className='flex flex-row w-full  justify-between items-center mt- 2xl:mt-10 '>
-        <div className='flex  justify-between items-center  flex-1  '><div className='relative ml-0 md:ml-5'> <img src='/assets/ZIDOC_Logo_Black_CMYK.svg' alt='' className='2xl:w-[129px] lg:w-[100px] w-[50px] ml-4 '/><img src='/assets/SecurityLockB.svg' alt='' className='w-[5px] absolute right-0 top-[-10px]'/> </div> </div>
+        <div className='flex  justify-between items-center  flex-1  '><div className='relative ml-0 md:ml-5'> <img src='/assets/ZIDOC_Logo_Black_CMYK.svg' alt='' className='2xl:w-[129px] lg:w-[100px] w-[50px] ml-8 '/><img src='/assets/SecurityLockB.svg' alt='' className='w-[5px] absolute right-0 top-[-10px]'/> </div> </div>
         <div className='flex flex-col justify-center items-center flex-1 absolute ml-20 md:ml-0 right-[350px] xl:right-[610px] lg:right-[500px]  2xl:right-[820px]'> <img src='/assets/ZigBlack.svg' alt='' className=' 2xl:w-[204px] lg:w-[190px] w-[120px] mr-5 ' /></div>
         <div className='flex flex-col justify-between items-end mt-3 mr-10 flex-1 '>
           <div className='flex flex-row justify-center items-center'><div className='mr-2 2xl:mr-8'><ClockComponent /> </div> 
---
+++ components/SideBar/SideBarPricing.jsx
@@ -0,0 +1,144 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useState } from 'react';
+import Drawer from 'react-modern-drawer';
+import 'react-modern-drawer/dist/index.css';
+import { useDispatch, useSelector } from "react-redux";
+import { useRouter } from 'next/router';
+
+const SideBarPricing = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  // const { sidebarstate } = useSelector((state) => state.custom);
+  const [isOpen, setIsOpen] = useState(false);
+
+
+  const sidebaropen = () => {
+    dispatch({ type: "sidebaropen" });
+};
+const sidebaroff = () => {
+    dispatch({ type: "sidebaroff" });
+};
+
+  const handleSidebarToggle = () => {
+    setIsOpen(!isOpen);
+    sidebaropen();
+  };
+
+  const handleOverlayClick = () => {
+    setIsOpen(false);
+    sidebaroff();
+  };
+
+  const handleSidebarItemClick = (route) => {
+    router.push(route);
+    setIsOpen(false);
+  };
+
+  const sidebarStyle = {
+    width: isOpen ? '300px' : '40px', // Adjust the width when open or closed
+    transition: 'width 0.2s ease-in-out',
+    backgroundColor: isOpen ? 'black' : 'transparent', // Set the background color to red when open, transparent when closed
+  };
+
+  const getContent = () => {
+    if (!isOpen) {
+      return (
+<div className='bg-black  text-white flex flex-col h-[90px] 2xl:h-[100px]  w-[40px] justify-start items-start overflow-y-hidden' onClick={handleSidebarToggle}>
+
+  <div className='h-[80px] 2xl:h-[100px] w-full  flex flex-col justify-center items-center  mt-10'>
+<div className='transform -rotate-90 origin-center text-[11px] uppercase border-r-2 border-golden  pr-2 tracking-[2px] mt-[-40px]'>  Pricing</div>
+  </div>
+  
+</div>
+
+
+      );
+    }
+
+    return null;
+  };
+
+  return (
+    <div>
+      {/* <button onClick={handleSidebarToggle}>Toggle Sidebar</button> */}
+      <Drawer
+        open={isOpen}
+        onClose={handleOverlayClick}
+        direction="left"
+        style={sidebarStyle}
+      >
+        <div className='h-[100vh] bg-blue-400 flex flex-col items-start ml-2 overflow-y-scroll'>
+          <div className='flex flex-col'>
+        {/* <img src='/assets/SideBarLogo.png' alt=''  className=' w-[183px] mt-2 2xl:mt-8 '/> */
+        }
+        <div className='flex items-end'>
+        <div className=' h-[130px] w-[25px]  flex flex-col justify-center items-center  '>
+<div className='transform -rotate-90 origin-center text-[14px] font-semibold uppercase border-l-2 border-[#BE9F56]  pl-2 pr-4 tracking-[3px] text-white'>products</div>
+  </div>
+        <img src='/assets/PoweredbyZiDocWB.svg' alt='' className='w-[129px] mb-2 ml-7'/>
+        </div>
+        {/* <img src='/assets/Products.png' alt=''  className=' w-[238px] mt-2'/> */}
+        <div className='uppercase tracking-[5px] text-[38px] 2xl:mt-2 text-[#333333]'>products</div>
+        </div>
+            <div className='mt-4 mr-2'>
+            <div className='w-full uppercase text-[10px] text-white font-normal text-right tracking-[2px] cursor-pointer' onClick={() => handleSidebarItemClick('/signatures')}> signatures</div>
+            <div className=' pl-[2px]'>
+            <div className='w-full uppercase text-[10px] text-white font-semibold text-left tracking-[2px] cursor-pointer my-2 2xl:m3-3 2xl:mb-2' onClick={() => handleSidebarItemClick('/signatures')}>electronic signature</div>
+              <div className='w-full uppercase text-[9px] text-white font-semibold text-left tracking-[2px] my-1 opacity-40'> Worlds most secure way to sign <br/> and send</div>
+            </div>
+            <div className=' pl-[2px]'>
+            <div className='w-full uppercase text-[10px] text-white font-semibold text-left tracking-[2px] cursor-pointer my-2 2xl:mt-3 2xl:mb-2' onClick={() => handleSidebarItemClick('/ewitness')}> e-witness</div>
+              <div className='w-full uppercase text-[9px] text-white font-semibold text-left tracking-[2px] my-1 opacity-40'>witness & sign confidential and<br/> high value agreements/contracts</div>
+            </div>
+            <div className=' pl-[2px]'>
+            <div className='w-full uppercase text-[10px] text-white font-semibold text-left tracking-[2px] cursor-pointer my-2 2xl:mt-3 2xl:mb-2' onClick={() => handleSidebarItemClick('/identityverification')}> identify verification</div>
+              <div className='w-full uppercase text-[9px] text-white font-semibold text-left tracking-[2px] my-1 opacity-40'>working seamlessly with aes <br/> 256 bit encryption and zimo meet</div>
+            </div>
+
+            <div className='w-full mr-1 uppercase text-[10px] text-white font-normal text-right tracking-[2px] cursor-pointer my-2 2xl:mt-3' onClick={() => handleSidebarItemClick('/contracts')}> contracts</div>
+            <div className=' mr-3'>
+            <div className='w-full uppercase text-[10px] text-white font-semibold text-right tracking-[2px] cursor-pointer my-2 2xl:mt-3 2xl:mb-2' onClick={() => handleSidebarItemClick('/contractagreement')}> contract/agreement management</div>
+              <div className='w-full pr-6 uppercase text-[9px] text-white font-semibold text-right tracking-[2px] my-1 opacity-40'>organised, automated document work flows</div>
+            </div>
+            <div className=' pl-[2px]'>
+            <div className='w-full uppercase text-[10px] text-white font-semibold text-left tracking-[2px] cursor-pointer my-2 2xl:mt-3 2xl:mb-2' onClick={() => handleSidebarItemClick('/documentgeneration')}> document generation</div>
+              <div className='w-full uppercase text-[9px] text-white font-semibold text-left tracking-[2px] my-1 opacity-40'>generate documents seamlessly through your dedicated portal</div>
+            </div>
+            <div className=' pl-[2px]'>
+            <div className='w-full uppercase text-[10px] text-white font-semibold text-left tracking-[2px] cursor-ponter my-2 2xl:mt-3 2xl:mb-2' onClick={() => handleSidebarItemClick('/datasecurity')}> data security</div>
+              <div className='w-full uppercase text-[9px] text-white  font-semibold text-left tracking-[2px] leading-4 opacity-40'>all signatures, contracts, agreements, documents and witness statements kept under aes 256-bit encryption for the duration of your membership</div>
+            </div>
+
+            <div className='w-full uppercase text-[10px] text-white font-semibold text-right tracking-[2px] cursor-pointer my-2 2xl:mt-3 2xl:mb-2' onClick={() => handleSidebarItemClick('/history')}> history</div>
+            <div className=' pl-[2px]'>
+            <div className='w-full uppercase text-[10px] text-white font-semibold text-left tracking-[2px] cursor-pointer my-2 2xl:my-3 2xl:mb-2' onClick={() => handleSidebarItemClick('/docsafe')}> doc.safe</div>
+              <div className='w-full uppercase text-[9px] text-white  font-semibold text-left tracking-[2px] my-1 opacity-40'> search, view, download and share every single signature, contract, agreement, and document from your portal</div>
+            </div>
+
+            <div className='w-full uppercase text-[10px] text-white font-semibold text-right tracking-[2px] cursor-pointer my-1' onClick={() => handleSidebarItemClick('/more')}> more</div>
+            <div className=' pl-[2px]'>
+            <div className='w-full uppercase text-[10px] text-white font-semibold text-left tracking-[2px] cursor-pointer' onClick={() => handleSidebarItemClick('/payment')}> payments </div>
+              <div className='w-full uppercase text-[9px] text-white  font-semibold text-left tracking-[2px] my-1 opacity-40'> agree, sign and make payment(s) in a single click</div>
+            </div>
+            <div className=' pl-[2px]'>
+            <div className='w-full uppercase text-[10px] text-white font-semibold text-left tracking-[2px] cursor-pointer my-2 2xl:mt-3 2xl:mb-2' onClick={() => handleSidebarItemClick('/clickwraps')}> clickwraps </div>
+              <div className='w-full uppercase text-[9px] text-white  font-semibold text-left tracking-[2px] my-1 opacity-40'> capture consent with a single click</div>
+            </div>
+            <div className=' pl-[2px]'>
+            <div className='w-full uppercase text-[10px] text-white font-semibold text-left tracking-[2px] cursor-pointer my-2 2xl:mt-3 2xl:mb-2' onClick={() => handleSidebarItemClick('/webforms')}> web forms </div>
+              <div className='w-full uppercase text-[9px] text-white font-semibold text-left tracking-[2px] my-1 opacity-40'> incorporate data collection and <br/> speed  up signing</div>
+            </div>
+
+            <div className='w-full uppercase text-[10px] text-white font-semibold text-right tracking-[2px] flex flex-row justify-end  cursor-pointer mt-6' onClick={() => handleSidebarItemClick('/allproducts')}> all products <img src='/assets/SideBarRightArrow.png' alt='' className='ml-5' /> </div>
+
+
+            </div>
+            
+            Sidebar Content</div>
+      </Drawer>
+      {getContent()}
+    </div>
+  );
+};
+
+export default SideBarPricing;
